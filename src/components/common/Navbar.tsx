474ed9f253432eca33e9b45a506cd062cd825a63
velut <12040076+velut@users.noreply.github.com>
2020-08-22 18:59:39 +0200

    feat: update NavbarLogo component

--- src/components/common/Navbar.tsx
+++ src/components/common/Navbar.tsx
@@ -2,14 +2,10 @@ import dynamic from 'next/dynamic';
 import React from 'react';
 import { DarkModeHook } from '../../hooks/useDarkMode';
 import { NavbarLinks } from './NavbarLinks';
+import { NavbarLogo } from './NavbarLogo';
 
-// Import as dynamic components, otherwise client props may differ
+// Import as dynamic component, otherwise client props may differ
 // from server rendered ones due to the dark mode state.
-const NavbarLogo = dynamic(
-    (() => import('./NavbarLogo').then(({ NavbarLogo }) => NavbarLogo)) as any,
-    { ssr: false }
-) as any;
-
 const ThemeButton = dynamic(
     (() =>
         import('./ThemeButton').then(({ ThemeButton }) => ThemeButton)) as any,
@@ -20,7 +16,7 @@ export function Navbar({ isDarkMode, toggleDarkMode }: DarkModeHook) {
     return (
         <header className="flex items-center justify-between px-4 py-3 bg-white border-b border-gray-300 dark:bg-gray-900 dark:border-gray-700">
             <div className="flex items-center">
-                <NavbarLogo isDarkMode={isDarkMode} />
+                <NavbarLogo />
                 <NavbarLinks />
             </div>
 
--- src/components/common/NavbarLogo.tsx
+++ src/components/common/NavbarLogo.tsx
@@ -1,16 +1,11 @@
 import React from 'react';
-import { DarkModeHook } from '../../hooks/useDarkMode';
 
-export function NavbarLogo({ isDarkMode }: Pick<DarkModeHook, 'isDarkMode'>) {
+export function NavbarLogo() {
     return (
         <a className="flex items-center" href="/" title="jsDocs.io">
-            <img
-                className="h-8"
-                src={isDarkMode ? '/logo-32-dark.png' : '/logo-32-light.png'}
-                alt="jsDocs.io logo"
-            />
+            <img className="h-12" src="/logo.png" alt="jsDocs.io logo" />
 
-            <span className="ml-2 text-2xl font-bold text-gray-900 dark:text-gray-100">
+            <span className="ml-3 text-2xl font-bold text-gray-900 dark:text-gray-100">
                 jsDocs.io
             </span>
         </a>
