f45fe129843c8565c4b4b41517712fae830c8840
velut <12040076+velut@users.noreply.github.com>
2020-08-27 16:45:30 +0200

    refactor: increase main container horizontal padding

--- src/components/common/Main.tsx
+++ src/components/common/Main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Main(props: any) {
     return (
         <main className="flex-grow bg-white dark:bg-gray-800">
-            <div className="container h-full px-4 py-12 mx-auto">
+            <div className="container h-full px-8 py-12 mx-auto">
                 {props.children}
             </div>
         </main>
