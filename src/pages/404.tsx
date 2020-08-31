139ebc2b1c007e240544a8f06870ae3677116cf2
velut <12040076+velut@users.noreply.github.com>
2020-08-31 18:35:30 +0200

    feat: add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,21 @@
+import Head from 'next/head';
+import React from 'react';
+import { Layout } from '../components/common/Layout';
+
+export default function My404() {
+    return (
+        <>
+            <Head>
+                <title>Page not found - jsDocs.io</title>
+                <meta name="description" content="Page not found - jsDocs.io" />
+            </Head>
+
+            <Layout>
+                <div className="flex flex-col items-center justify-center h-full">
+                    <h1 className="text-4xl font-bold">404</h1>
+                    <p className="text-xl">Page not found</p>
+                </div>
+            </Layout>
+        </>
+    );
+}
