c061f0ce516259aaa0f0a871033086ce8f5ca903
velut <12040076+velut@users.noreply.github.com>
2020-09-10 20:48:26 +0200

    feat: use PackageNav in PackagePageAvailableVersions

--- src/components/package/PackagePageAvailableVersions.tsx
+++ src/components/package/PackagePageAvailableVersions.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { PackagePagePropsAvailableVersions } from '../../lib/package-page-props';
 import { Layout } from '../common/Layout';
 import { PackageDistTags } from './PackageDistTags';
+import { PackageNav } from './PackageNav';
 import { PackagePageFooter } from './PackagePageFooter';
 import { PackageVersions } from './PackageVersions';
 
@@ -9,10 +10,16 @@ export function PackagePageAvailableVersions({
     packument,
     createdAt,
 }: PackagePagePropsAvailableVersions) {
-    const { name, distTags, versionsTimestamps } = packument;
+    const { name, repository, distTags, versionsTimestamps } = packument;
 
     return (
         <Layout>
+            <PackageNav
+                name={name}
+                repositoryURL={repository?.url}
+                hideInternalNav={true}
+            />
+
             <PackageDistTags name={name} distTags={distTags} />
 
             <PackageVersions
