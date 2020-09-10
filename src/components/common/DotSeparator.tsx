67f68b3dae3d2ddc85b81039eb5c9845a443f1fb
velut <12040076+velut@users.noreply.github.com>
2020-09-10 20:47:36 +0200

    feat: add PackageNav

---
+++ src/components/common/DotSeparator.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function DotSeparator() {
+    return (
+        <div className="px-1">
+            <span className="text-gray-600">&middot;</span>
+        </div>
+    );
+}
