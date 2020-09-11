baf53bc469f7cc34f0c95994bf639b10e5422772
velut <12040076+velut@users.noreply.github.com>
2020-09-11 14:47:01 +0200

    feat: add progress bar on route change

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 /* istanbul ignore file */
 
 import type { AppProps } from 'next/app';
+import Router from 'next/router';
+import NProgress from 'nprogress';
 import React from 'react';
+import '../../node_modules/nprogress/nprogress.css';
 import '../../node_modules/prismjs/themes/prism-tomorrow.css';
 import '../../styles/index.css';
+
+NProgress.configure({ showSpinner: false });
+
+Router.events.on('routeChangeStart', () => NProgress.start());
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
     return <Component {...pageProps} />;
