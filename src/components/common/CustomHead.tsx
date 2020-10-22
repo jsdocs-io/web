bd5d1178c171643c24f4b6ff5ba3e27450581a5f
velut <12040076+velut@users.noreply.github.com>
2020-10-22 17:48:49 +0200

    feat: add favicon

--- src/components/common/CustomHead.tsx
+++ src/components/common/CustomHead.tsx
@@ -11,6 +11,37 @@ export function CustomHead() {
                 content="width=device-width, initial-scale=1, shrink-to-fit=no"
             />
 
+            {/* Favicon */}
+            <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href="/apple-touch-icon.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href="/favicon-16x16.png"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href="/favicon-32x32.png"
+            />
+            <link rel="manifest" href="/site.webmanifest" />
+            <link
+                rel="mask-icon"
+                href="/safari-pinned-tab.svg"
+                color="#3182ce"
+            />
+            <meta name="msapplication-TileColor" content="#2d3748" />
+            <meta
+                name="msapplication-TileImage"
+                content="/mstile-144x144.png"
+            />
+            <meta name="theme-color" content="#2d3748" />
+
             {/* Global Open Graph tags */}
             <meta property="og:type" content="website" />
             <meta
