e4a1811d7ed2138f5e2bae822eb85d1f85a4ef84
velut <12040076+velut@users.noreply.github.com>
2020-10-29 17:50:39 +0100

    build: add webpack stats visualizer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,28 @@
+const { StatsWriterPlugin } = require('webpack-stats-plugin');
+const Visualizer = require('webpack-visualizer-plugin');
+
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
     enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({});
+module.exports = withBundleAnalyzer({
+    webpack: (config, {}) => {
+        config.plugins.push(
+            new StatsWriterPlugin({
+                filename: 'stats.json',
+                stats: {
+                    context: __dirname,
+                    assets: true,
+                    entrypoints: true,
+                    chunks: true,
+                    chunkModules: true,
+                    modules: true,
+                },
+            })
+        );
+
+        config.plugins.push(new Visualizer());
+
+        return config;
+    },
+});
