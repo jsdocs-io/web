823a272d3aee84e5682011bde5bccd0320068898
velut <12040076+velut@users.noreply.github.com>
2020-11-16 19:20:41 +0100

    test: remove unused tests

--- test/lib/package-analyzer.test.ts
+++
@@ -1,7 +0,0 @@
-import { packageAnalyzer } from '../../src/lib/package-analyzer';
-
-describe('PackageAnalyzer instance', () => {
-    it('should be defined', () => {
-        expect(packageAnalyzer).toBeDefined();
-    });
-});
--- test/lib/registry.test.ts
+++
@@ -1,7 +0,0 @@
-import { registry } from '../../src/lib/registry';
-
-describe('Registry instance', () => {
-    it('should be defined', () => {
-        expect(registry).toBeDefined();
-    });
-});
