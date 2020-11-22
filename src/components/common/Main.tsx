47fe941d0a34d3594465c573917c05477a58835c
velut <12040076+velut@users.noreply.github.com>
2020-11-22 19:19:07 +0100

    refactor: update main container padding

--- src/components/common/Main.tsx
+++ src/components/common/Main.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export function Main(props: any) {
     return (
         <main className="flex-grow bg-white dark:bg-gray-800">
-            <div className="container h-full px-8 py-12 mx-auto">
+            <div className="container h-full max-w-screen-xl px-4 py-12 mx-auto sm:px-8 lg:px-12 xl:px-20">
                 {props.children}
             </div>
         </main>
