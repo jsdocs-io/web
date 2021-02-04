06a6bead4e90edc0249192a7542398d0a85df89e
velut <12040076+velut@users.noreply.github.com>
2021-02-04 19:44:37 +0100

    feat: highlight index file

--- src/components/package/PackageFilesList.tsx
+++ src/components/package/PackageFilesList.tsx
@@ -6,17 +6,50 @@ import { A } from '../common/A';
 export function PackageFilesList({ files }: { files: PackageFile[] }) {
     return (
         <ul className="mt-2 list-inline">
-            {files.map(({ filename, url }) => (
+            {files.map(({ filename, url, isIndexFile }) => (
                 <li key={filename}>
-                    {url && isRepositoryFile({ filename }) ? (
-                        <A href={url} title="View source file">
-                            {filename}
-                        </A>
-                    ) : (
-                        <>{filename}</>
-                    )}
+                    <Filename
+                        filename={filename}
+                        url={url}
+                        isIndexFile={isIndexFile}
+                    />
                 </li>
             ))}
         </ul>
+    );
+}
+
+function Filename({
+    filename,
+    url,
+    isIndexFile,
+}: {
+    filename: string;
+    url?: string;
+    isIndexFile?: boolean;
+}) {
+    // Can link to source
+    if (url && isRepositoryFile({ filename })) {
+        return (
+            <A href={url} title="View source file">
+                {isIndexFile ? (
+                    <span className="font-bold hover:underline">
+                        {filename}
+                    </span>
+                ) : (
+                    <>{filename}</>
+                )}
+            </A>
+        );
+    }
+
+    return (
+        <>
+            {isIndexFile ? (
+                <span className="font-bold">{filename}</span>
+            ) : (
+                <>{filename}</>
+            )}
+        </>
     );
 }
