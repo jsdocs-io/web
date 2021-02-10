c218933121bb0ad6ba97d4a8755b8ad42940dd2b
velut <12040076+velut@users.noreply.github.com>
2021-02-10 23:29:00 +0100

    feat: improve word break on links and inline code

--- src/components/common/InlineCode.tsx
+++ src/components/common/InlineCode.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export function InlineCode({ code }: { code: string }) {
     return (
-        <code className="text-pink-800 break-all dark:text-pink-300">
+        <code className="text-pink-800 break-all sm:break-normal dark:text-pink-300">
             {code}
         </code>
     );
