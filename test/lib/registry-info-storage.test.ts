7d0d19b861a3ea1322235123da596d1db71aca42
velut <12040076+velut@users.noreply.github.com>
2021-02-11 20:01:41 +0100

    test: always use mock client for storage tests

--- test/lib/registry-info-storage.test.ts
+++ test/lib/registry-info-storage.test.ts
@@ -1,11 +1,25 @@
+import { mocked } from 'ts-jest/utils';
 import {
     loadRegistryPackageInfo,
     storeRegistryPackageInfo,
 } from '../../src/lib/registry-package-info-storage';
+import { loadObject, storeObject } from '../../src/lib/storage';
+
+jest.mock('../../src/lib/storage', () => ({
+    loadObject: jest.fn(),
+    storeObject: jest.fn(),
+}));
+const mockedLoadObject = mocked(loadObject, true);
+const mockedStoreObject = mocked(storeObject, true);
 
 describe('loadRegistryPackageInfo', () => {
     it('returns undefined if an object cannot be retrieved', async () => {
         expect.assertions(1);
+
+        mockedLoadObject.mockImplementation(async () => {
+            return undefined;
+        });
+
         const obj = await loadRegistryPackageInfo({
             name: 'foo',
             version: '99.0.0',
@@ -17,6 +31,10 @@ describe('loadRegistryPackageInfo', () => {
 describe('storeRegistryPackageInfo', () => {
     it('tries to store an object', async () => {
         expect.assertions(1);
+
+        mockedStoreObject.mockImplementation(async () => {
+            return;
+        });
 
         try {
             await storeRegistryPackageInfo({
--- test/lib/storage.test.ts
+++ test/lib/storage.test.ts
@@ -2,6 +2,13 @@ import stream from 'stream';
 import { loadObject, storeObject } from '../../src/lib/storage';
 
 describe('loadObject', () => {
+    it('uses the default client by default', async () => {
+        expect.assertions(1);
+
+        const obj = await loadObject({ name: '' });
+        expect(obj).toBeUndefined();
+    });
+
     it('returns the object found in storage and parsed from JSON', async () => {
         expect.assertions(1);
 
@@ -36,18 +43,42 @@ describe('loadObject', () => {
 
     it('returns undefined if an object cannot be retrieved', async () => {
         expect.assertions(1);
-        const obj = await loadObject({ name: 'foo' });
+
+        const mockClient = {
+            async getObject() {
+                throw new Error('object not found');
+            },
+            async putObject() {
+                throw new Error('not implemented');
+            },
+        };
+
+        const obj = await loadObject({ name: 'foo', client: mockClient });
         expect(obj).toBeUndefined();
     });
 });
 
 describe('storeObject', () => {
+    it('uses the default client by default', async () => {
+        expect.assertions(1);
+
+        await storeObject({ name: '', obj: {} });
+        expect(true).toBeTruthy();
+    });
+
     it('tries to store an object', async () => {
         expect.assertions(1);
 
-        try {
-            await storeObject({ name: 'foo', obj: {} });
-            expect(true).toBeTruthy();
-        } catch {}
+        const mockClient = {
+            async getObject() {
+                throw new Error('not implemented');
+            },
+            async putObject() {
+                throw new Error('operation failed');
+            },
+        };
+
+        await storeObject({ name: 'foo', obj: {}, client: mockClient });
+        expect(true).toBeTruthy();
     });
 });
