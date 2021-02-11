2d0838b0e241e7cdbbb2a731c546deadcbb2eb01
velut <12040076+velut@users.noreply.github.com>
2021-02-11 23:53:04 +0100

    fix: use unique keys in PackageIndexNamespaceMembersList
    Namespaces can contain merged declarations
    with the same ID but different kinds
    (for example, a variable and a type alias sharing the same name).

--- src/components/package/PackageIndexNamespaceMembersList.tsx
+++ src/components/package/PackageIndexNamespaceMembersList.tsx
@@ -16,7 +16,7 @@ export function PackageIndexNamespaceMembersList({
     return (
         <ul className="pl-4 mt-1 ml-5 border-l border-gray-300 dark:border-gray-700">
             {members.map(({ id, name, kind }) => (
-                <li key={id}>
+                <li key={`${id}-${kind}`}>
                     <InternalLink
                         href={`#${id}`}
                         title={`${getDeclarationKindDescription({
