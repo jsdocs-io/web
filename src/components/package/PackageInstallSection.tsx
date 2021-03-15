28881c6e3e68eb615f92066dcfcb6e909de53af8
velut <12040076+velut@users.noreply.github.com>
2021-03-15 22:31:10 +0100

    refactor: update PackageInstallSection

--- src/components/package/PackageInstallSection.tsx
+++ src/components/package/PackageInstallSection.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import { CodeBlock } from '../common/CodeBlock';
 
 export function PackageInstallSection({ name }: { name: string }) {
+    const installCommands = [
+        `npm i ${name}`,
+        `yarn add ${name}`,
+        `pnpm add ${name}`,
+    ];
+
     return (
         <section className="space-y-4">
             <h2 id="package-install">Install</h2>
 
-            <CodeBlock code={`npm i ${name}`} language="bash" />
-            <CodeBlock code={`yarn add ${name}`} language="bash" />
-            <CodeBlock code={`pnpm add ${name}`} language="bash" />
+            {installCommands.map((command) => (
+                <CodeBlock key={command} code={command} language="bash" />
+            ))}
         </section>
     );
 }
