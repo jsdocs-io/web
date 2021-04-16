c95b3666b390c2480fe400bb74ba8fd0669fd382
velut <12040076+velut@users.noreply.github.com>
2021-04-16 20:46:26 +0200

    feat: timeout package analysis after 55 seconds

--- src/lib/get-registry-package-info.ts
+++ src/lib/get-registry-package-info.ts
@@ -1,4 +1,5 @@
 import { RegistryPackageInfo } from '@jsdocs-io/extractor';
+import EventEmitter from 'events';
 import path from 'path';
 import Piscina from 'piscina';
 import {
@@ -26,7 +27,18 @@ export async function getRegistryPackageInfo({
     }
 
     // Run `analyzeRegistryPackage({ name, version })` in a separate worker thread
-    const info: RegistryPackageInfo = await piscina.runTask({ name, version });
+    // and timeout after 55 seconds (Vercel limits functions to 60 seconds)
+    const abortEmitter = new EventEmitter();
+    const analyzeRegistryPackageTask = piscina.runTask(
+        { name, version },
+        abortEmitter
+    );
+    const timeout = setTimeout(() => {
+        abortEmitter.emit('abort');
+    }, 55000);
+
+    const info: RegistryPackageInfo = await analyzeRegistryPackageTask;
+    clearTimeout(timeout);
 
     await storeRegistryPackageInfo({ name, version, info });
 
