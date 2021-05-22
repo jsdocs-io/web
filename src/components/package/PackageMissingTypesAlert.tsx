b668b727d85d0c4f6be800823b4b659004ba3398
velut <12040076+velut@users.noreply.github.com>
2021-05-22 13:09:10 +0200

    refactor: update PackageMissingTypesAlert text

--- src/components/package/PackageMissingTypesAlert.tsx
+++ src/components/package/PackageMissingTypesAlert.tsx
@@ -4,7 +4,8 @@ export function PackageMissingTypesAlert() {
     return (
         <div className="flex justify-center p-4 border-2 border-yellow-500 rounded">
             <p className="mt-0 font-bold">
-                Unfortunately no type definitions are available for this package
+                Unfortunately type definitions are not available for this
+                package
             </p>
         </div>
     );
