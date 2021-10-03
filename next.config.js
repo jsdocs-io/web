618cd65bc283b2239bacf438ff5b7a818b68e01e
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2021-10-03 19:40:48 +0200

    chore: revert "feat: setup sentry.io"
    This reverts commit 20b8f241050e3cf7fcf8ba753f90e701a24701da.

--- next.config.js
+++ next.config.js
@@ -1,4 +1,3 @@
-const { withSentryConfig } = require('@sentry/nextjs');
 const { LicenseWebpackPlugin } = require('license-webpack-plugin');
 const { StatsWriterPlugin } = require('webpack-stats-plugin');
 const Visualizer = require('webpack-visualizer-plugin2');
@@ -47,7 +46,7 @@ const statsPlugin = new StatsWriterPlugin({
 
 const visualizerPlugin = new Visualizer();
 
-const moduleExports = withBundleAnalyzer({
+module.exports = withBundleAnalyzer({
     webpack: (config, { isServer }) => {
         const isClient = !isServer;
 
@@ -61,19 +60,3 @@ const moduleExports = withBundleAnalyzer({
         return config;
     },
 });
-
-const SentryWebpackPluginOptions = {
-    // Additional config options for the Sentry Webpack plugin. Keep in mind that
-    // the following options are set automatically, and overriding them is not
-    // recommended:
-    //   release, url, org, project, authToken, configFile, stripPrefix,
-    //   urlPrefix, include, ignore
-
-    silent: true, // Suppresses all logs
-    // For all available options, see:
-    // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
-
-// Make sure adding Sentry options is the last code to run before exporting, to
-// ensure that your source maps include changes from all other Webpack plugins
-module.exports = withSentryConfig(moduleExports, SentryWebpackPluginOptions);
