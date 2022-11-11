a361f18e768e62cfe3ea2d8ae4354110f89ff2ce
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2022-11-11 20:13:54 +0100

    feat: add Logo component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,26 @@
+import clsx from "clsx";
+import Link from "next/link";
+
+const Logo = ({ hideText = false }: { hideText?: boolean }) => {
+  return (
+    <Link href="/">
+      <a className="flex flex-none items-center space-x-2" title="jsDocs.io">
+        <img
+          className="h-8 flex-none"
+          src="/logo.png"
+          alt="Logo for jsDocs.io"
+        />
+        <span
+          className={clsx(
+            "text-2xl font-bold",
+            hideText ? "hidden sm:block" : "block"
+          )}
+        >
+          jsDocs.io
+        </span>
+      </a>
+    </Link>
+  );
+};
+
+export default Logo;
