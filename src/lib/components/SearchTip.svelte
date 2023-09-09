2b0e5f64e0d0a4c87e21d8139ac01da85acaeccd
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-09 20:57:43 +0200

    feat: add SearchTip

---
+++ src/lib/components/SearchTip.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	const packages = ['query-registry', '@types/prettier'];
+</script>
+
+<p class="prose">
+	<strong>Tip:</strong> Search for npm packages, for example
+	<a href="/package/{packages[0]}">{packages[0]}</a> or
+	<a href="/package/{packages[1]}">{packages[1]}</a>.
+</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import SearchBar from '$lib/components/SearchBar.svelte';
+	import SearchTip from '../lib/components/SearchTip.svelte';
 </script>
 
 <SearchBar />
+<SearchTip />
