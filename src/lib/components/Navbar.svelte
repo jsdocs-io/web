c299a3a87bcca3d0ed2bbcfbc8da12462f1e9c04
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-09 19:12:50 +0200

    feat: add global layout with navbar and footer

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,30 @@
+<div class="navbar bg-base-100">
+	<div class="flex-1">
+		<a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+	</div>
+	<div class="flex-none gap-2">
+		<div class="form-control">
+			<input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+		</div>
+		<div class="dropdown dropdown-end">
+			<label tabindex="0" class="btn btn-ghost btn-circle avatar">
+				<div class="w-10 rounded-full">
+					<img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+				</div>
+			</label>
+			<ul
+				tabindex="0"
+				class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+			>
+				<li>
+					<a class="justify-between">
+						Profile
+						<span class="badge">New</span>
+					</a>
+				</li>
+				<li><a>Settings</a></li>
+				<li><a>Logout</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,15 @@
 <script>
+	import Footer from '$lib/components/Footer.svelte';
+	import Navbar from '$lib/components/Navbar.svelte';
 	import '../app.css';
 </script>
 
-<slot />
+<div class="flex h-screen flex-col">
+	<Navbar />
+
+	<div class="grow">
+		<slot />
+	</div>
+
+	<Footer />
+</div>
