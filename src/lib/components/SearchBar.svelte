a4ea6522caaf8176812dc3ba019ffd01821a48a8
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-11 03:24:16 +0200

    feat: update homepage and search components

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -2,11 +2,13 @@
 	import SearchIcon from '$lib/icons/SearchIcon.svelte';
 </script>
 
-<div class="join">
+<div class="join w-full">
 	<input
 		type="text"
 		placeholder="Search npm packages"
-		class="input join-item input-bordered input-lg w-full max-w-xs"
+		class="input join-item input-bordered input-success input-lg w-full"
 	/>
-	<button class="btn btn-square join-item btn-lg"><SearchIcon class="h-7 w-7" /></button>
+	<button class="btn btn-square btn-success join-item btn-lg"
+		><SearchIcon class="h-7 w-7 text-white" /></button
+	>
 </div>
--- src/lib/components/SearchTip.svelte
+++ src/lib/components/SearchTip.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	const packages = ['query-registry', '@types/prettier'];
+	const packages = ['faastjs', 'prettier', '@types/html-minifier'];
 </script>
 
-<p class="prose">
-	<strong>Tip:</strong> Search for npm packages, for example
-	<a href="/package/{packages[0]}">{packages[0]}</a> or
-	<a href="/package/{packages[1]}">{packages[1]}</a>.
+<p class="prose text-lg">
+	<strong>Tip:</strong> Try with packages like
+	<a href="/package/{packages[0]}">{packages[0]}</a>,
+	<a href="/package/{packages[1]}">{packages[1]}</a> or
+	<a href="/package/{packages[2]}">{packages[2]}</a>
 </p>
