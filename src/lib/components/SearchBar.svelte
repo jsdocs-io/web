947d23ceefe7af9fee08ee639c1e29fb8e116692
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-11 02:10:22 +0200

    refactor: change search icon

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,5 +1,5 @@
 <script>
-	import RoundSearch from '$lib/icons/RoundSearch.svelte';
+	import SearchIcon from '$lib/icons/SearchIcon.svelte';
 </script>
 
 <div class="join">
@@ -8,5 +8,5 @@
 		placeholder="Search npm packages"
 		class="input input-bordered input-lg w-full max-w-xs join-item"
 	/>
-	<button class="btn join-item btn-square btn-lg"><RoundSearch class="w-7 h-7" /></button>
+	<button class="btn join-item btn-square btn-lg"><SearchIcon class="w-7 h-7" /></button>
 </div>
