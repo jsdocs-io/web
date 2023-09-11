45ca5d22fdbfe7181e1c53da6504c04c7acadf96
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-11 02:20:16 +0200

    feat: add links and theme button to homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,20 @@
 <script lang="ts">
+	import FaqLinkButton from '$lib/components/FaqLinkButton.svelte';
+	import GitHubLinkButton from '$lib/components/GitHubLinkButton.svelte';
 	import SearchBar from '$lib/components/SearchBar.svelte';
-	import SearchTip from '../lib/components/SearchTip.svelte';
+	import SearchTip from '$lib/components/SearchTip.svelte';
+	import SponsorLinkButton from '$lib/components/SponsorLinkButton.svelte';
+	import ThemeButton from '$lib/components/ThemeButton.svelte';
 </script>
 
-<SearchBar />
-<SearchTip />
+<div class="h-screen p-4">
+	<div class="flex items-center justify-end gap-2">
+		<FaqLinkButton />
+		<SponsorLinkButton />
+		<GitHubLinkButton />
+		<ThemeButton />
+	</div>
+
+	<SearchBar />
+	<SearchTip />
+</div>
