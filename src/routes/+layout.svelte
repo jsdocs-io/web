020189fbfef9d5b436cac3a09b240b681320d740
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-12 02:12:42 +0200

    feat: setup nprogress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script lang="ts">
+	import { navigating } from '$app/stores';
+	import NProgress from 'nprogress';
+	import '../../node_modules/nprogress/nprogress.css';
 	import '../app.css';
+
+	NProgress.configure({ showSpinner: false });
+	$: {
+		if ($navigating) {
+			NProgress.start();
+		} else {
+			NProgress.done();
+		}
+	}
 </script>
 
 <slot />
