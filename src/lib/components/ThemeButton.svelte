8f3b8afa827f73004192f99b8a4dc88eabf6a4a1
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-12 02:01:27 +0200

    feat: implement theme detection and changing

--- src/lib/components/ThemeButton.svelte
+++ src/lib/components/ThemeButton.svelte
@@ -1,8 +1,18 @@
 <script lang="ts">
 	import FormatPaintIcon from '$lib/icons/FormatPaintIcon.svelte';
+
+	const changeTheme = () => {
+		if (document.documentElement.getAttribute('data-theme') === 'dark') {
+			localStorage.theme = 'light';
+			document.documentElement.setAttribute('data-theme', 'light');
+		} else {
+			localStorage.theme = 'dark';
+			document.documentElement.setAttribute('data-theme', 'dark');
+		}
+	};
 </script>
 
-<button class="btn btn-ghost" title="Change theme">
+<button type="button" class="btn btn-ghost" title="Change theme" on:click={changeTheme}>
 	<FormatPaintIcon class="h-6 w-6" />
 	Theme
 </button>
