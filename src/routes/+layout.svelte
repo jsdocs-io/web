52b146c40f1b774ba603fc7545da850c5e1d53a8
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-12 02:59:23 +0200

    feat: use customized nprogress theme

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { navigating } from '$app/stores';
 	import NProgress from 'nprogress';
-	import '../../node_modules/nprogress/nprogress.css';
 	import '../app.css';
 
 	NProgress.configure({ showSpinner: false });
