ff6502b991274fef5e5a4b86f2360390583b2e65
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-13 18:06:30 +0200

    refactor: add max width to SearchBar container

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -1,12 +1,12 @@
-<script>
+<script lang="ts">
 	import SearchIcon from '$lib/icons/SearchIcon.svelte';
 </script>
 
-<div class="join flex w-full justify-center">
+<div class="join w-full max-w-xl">
 	<input
 		type="text"
 		placeholder="Search npm packages"
-		class="input join-item input-bordered input-lg w-full max-w-lg border-base-content"
+		class="input join-item input-bordered input-lg w-full border-base-content"
 	/>
 	<button class="btn btn-square btn-outline join-item btn-lg" title="Search"
 		><SearchIcon class="h-7 w-7" /></button
