3675ee48bb74a1367ad1eb655c1aa34fff06de41
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-13 16:39:20 +0200

    feat: add homepage button to navbar

---
+++ src/lib/components/HomepageLinkButton.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import PackageIcon from '$lib/icons/PackageIcon.svelte';
+</script>
+
+<a href="/" class="btn btn-ghost" title="Home">
+	<PackageIcon class="h-6 w-6" />
+	jsDocs.io</a
+>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
 	import FaqLinkButton from '$lib/components/FaqLinkButton.svelte';
 	import GitHubLinkButton from '$lib/components/GitHubLinkButton.svelte';
+	import HomepageLinkButton from '$lib/components/HomepageLinkButton.svelte';
 	import SponsorLinkButton from '$lib/components/SponsorLinkButton.svelte';
 	import ThemeButton from '$lib/components/ThemeButton.svelte';
 </script>
 
-<div class="flex items-center gap-2 overflow-x-scroll p-4 sm:justify-end sm:overflow-x-auto">
-	<FaqLinkButton />
-	<SponsorLinkButton />
-	<GitHubLinkButton />
-	<ThemeButton />
+<div class="flex items-center justify-between gap-2 overflow-x-auto p-4">
+	<HomepageLinkButton />
+
+	<div class="flex items-center gap-2">
+		<FaqLinkButton />
+		<SponsorLinkButton />
+		<GitHubLinkButton />
+		<ThemeButton />
+	</div>
 </div>
