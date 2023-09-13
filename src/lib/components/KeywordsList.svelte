eaa5e94a6e3aadcbd158bcadedd47c2ce55d394a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-13 18:17:07 +0200

    feat: render KeywordsList only if there are keywords

--- src/lib/components/KeywordsList.svelte
+++ src/lib/components/KeywordsList.svelte
@@ -4,10 +4,12 @@
 	$: shortKeywords = keywords.slice(0, 7);
 </script>
 
-<ul class="flex flex-wrap gap-2">
-	{#each shortKeywords as keyword}
-		<li>
-			<a class="btn btn-xs font-normal normal-case" href="/search?q={keyword}">{keyword}</a>
-		</li>
-	{/each}
-</ul>
+{#if shortKeywords.length > 0}
+	<ul class="flex flex-wrap gap-2">
+		{#each shortKeywords as keyword}
+			<li>
+				<a class="btn btn-xs font-normal normal-case" href="/search?q={keyword}">{keyword}</a>
+			</li>
+		{/each}
+	</ul>
+{/if}
