a69311ff15823e44b784e026669dc212332d8a17
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-13 18:23:42 +0200

    refactor: wrap SearchResultTitle items to prevent overflow

--- src/lib/components/SearchResultTitle.svelte
+++ src/lib/components/SearchResultTitle.svelte
@@ -6,7 +6,7 @@
 	export let query: string;
 </script>
 
-<div class="flex items-center gap-4">
+<div class="flex flex-wrap items-center gap-4">
 	<a class="link-hover link block leading-none" href="/package/{name}">
 		<h3 class="block text-2xl font-bold leading-none">{name}</h3>
 	</a>
