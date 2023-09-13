f12dc3e51b7400455ed8a3ff7452fdce452124d4
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-13 20:11:23 +0200

    feat: add form to SearchBar for searching

--- src/lib/components/SearchBar.svelte
+++ src/lib/components/SearchBar.svelte
@@ -3,14 +3,15 @@
 </script>
 
 <label class="sr-only" for="search-bar">Search npm packages</label>
-<div class="join w-full max-w-xl">
+<form class="join w-full max-w-xl" action="/search">
 	<input
 		id="search-bar"
 		type="search"
+		name="q"
 		placeholder="Search npm packages"
 		class="input join-item input-bordered input-lg w-full border-base-content"
 	/>
-	<button class="btn btn-square btn-outline join-item btn-lg" title="Search"
+	<button class="btn btn-square btn-outline join-item btn-lg" title="Search" type="submit"
 		><SearchIcon class="h-7 w-7" /></button
 	>
-</div>
+</form>
