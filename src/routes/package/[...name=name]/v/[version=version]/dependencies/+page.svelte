d6cc03693ab822a2155701c90edd05dab6c92be7
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-15 22:07:22 +0200

    feat: add dependencies page for versioned package

---
+++ src/routes/package/[...name=name]/v/[version=version]/dependencies/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: ({ name, version, dependencies } = data);
+</script>
+
+<h1>Dependencies for package {name} version {version}</h1>
+
+<pre>{JSON.stringify(dependencies, null, 2)}</pre>
