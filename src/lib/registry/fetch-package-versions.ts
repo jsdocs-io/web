06fb984aafa56da0c77a7d9af3332a20b47993d5
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-18 18:40:10 +0200

    feat: update fetchPackageVersions
    Update schema.
    Use cache.

--- src/lib/registry/fetch-package-versions.ts
+++ src/lib/registry/fetch-package-versions.ts
@@ -1,11 +1,16 @@
 import { REGISTRY_API_URL } from '$env/static/private';
+import { fetchWithCache } from '$lib/registry/fetch-with-cache';
 import { z } from 'zod';
 
 const endpoint = REGISTRY_API_URL;
 
 const packageVersionsSchema = z
 	.object({
-		'dist-tags': z.record(z.string()),
+		'dist-tags': z
+			.object({
+				latest: z.string()
+			})
+			.catchall(z.string()),
 		time: z
 			.object({
 				created: z.string(),
@@ -18,7 +23,7 @@ const packageVersionsSchema = z
 		const { created, modified, ...history } = obj.time;
 		return {
 			tags: obj['dist-tags'],
-			history: history as Record<string, string>
+			history: history
 		};
 	});
 
@@ -28,10 +33,6 @@ export const fetchPackageVersions = async (
 	fetch: typeof window.fetch,
 	name: string
 ): Promise<PackageVersions> => {
-	const response = await fetch(`${endpoint}/${name}`);
-	if (!response.ok) {
-		throw new Error(response.statusText);
-	}
-	const data = await response.json();
+	const data = await fetchWithCache(fetch, `${endpoint}/${name}`);
 	return packageVersionsSchema.parse(data);
 };
