ba219764f65ae2e456b03b38153d80673a4aea58
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-09-18 20:04:18 +0200

    refactor: use fetchPackument

--- src/lib/registry/fetch-package-versions.ts
+++ src/lib/registry/fetch-package-versions.ts
@@ -1,22 +1,11 @@
-import { REGISTRY_API_URL } from '$env/static/private';
-import { fetchWithCache } from '$lib/registry/fetch-with-cache';
-import { z } from 'zod';
+import { fetchPackument } from '$lib/registry/fetch-packument';
+import { packumentSchema } from '$lib/registry/packument';
+import type { z } from 'zod';
 
-const endpoint = REGISTRY_API_URL;
-
-const packageVersionsSchema = z
-	.object({
-		'dist-tags': z
-			.object({
-				latest: z.string()
-			})
-			.catchall(z.string()),
-		time: z
-			.object({
-				created: z.string(),
-				modified: z.string()
-			})
-			.catchall(z.string())
+const packageVersionsSchema = packumentSchema
+	.pick({
+		'dist-tags': true,
+		time: true
 	})
 	.transform((obj) => {
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -33,6 +22,6 @@ export const fetchPackageVersions = async (
 	fetch: typeof window.fetch,
 	name: string
 ): Promise<PackageVersions> => {
-	const data = await fetchWithCache(fetch, `${endpoint}/${name}`);
-	return packageVersionsSchema.parse(data);
+	const packument = await fetchPackument(fetch, name);
+	return packageVersionsSchema.parse(packument);
 };
