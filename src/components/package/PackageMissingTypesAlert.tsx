97e010680f3abb9522d0eede986c0d5bb756c4cf
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-10-04 00:57:16 +0200

    refactor: update missing api error

--- src/components/package/PackageMissingTypesAlert.tsx
+++ src/components/package/PackageMissingTypesAlert.tsx
@@ -2,7 +2,8 @@ const PackageMissingTypesAlert = () => {
   return (
     <div className="flex justify-center p-4 border-2 border-yellow-500 rounded">
       <p className="mt-0 font-bold">
-        Unfortunately type definitions are not available for this package
+        Unfortunately type definitions are currently not available for this
+        package
       </p>
     </div>
   );
