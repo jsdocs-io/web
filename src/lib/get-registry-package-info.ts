e4beeb56dccbb1c3af95e98cdfa2a93faca49669
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-10-09 18:58:24 +0200

    feat: add a list of allowed packages for analysis

--- src/lib/get-registry-package-info.ts
+++ src/lib/get-registry-package-info.ts
@@ -1,5 +1,43 @@
-import { RegistryPackageInfo } from "@jsdocs-io/extractor";
-import { loadRegistryPackageInfo } from "./registry-package-info-storage";
+import {
+  RegistryPackageInfo,
+  analyzeRegistryPackage,
+} from "@jsdocs-io/extractor";
+import {
+  loadRegistryPackageInfo,
+  storeRegistryPackageInfo,
+} from "./registry-package-info-storage";
+
+const allowedPackages = new Set([
+  "@jsdocs-io/extractor",
+  "@types/react",
+  "@unocss/core",
+  "@violentmonkey/shortcut",
+  "axios",
+  "bundle-require",
+  "class-transformer",
+  "class-validator",
+  "depextract",
+  "enttec-open-dmx-usb",
+  "exome",
+  "faastjs",
+  "got",
+  "h3",
+  "hn-ts",
+  "iron-webcrypto",
+  "next",
+  "prettier",
+  "query-registry",
+  "react-router-dom",
+  "short-time-ago",
+  "string-ts",
+  "talkify-tts-api",
+  "tinyargs",
+  "verify-hcaptcha",
+  "vite",
+  "vue",
+  "yup",
+  "zod",
+]);
 
 const getRegistryPackageInfo = async ({
   name,
@@ -13,15 +51,13 @@ const getRegistryPackageInfo = async ({
     return cachedInfo;
   }
 
-  throw new Error(
-    "FIXME: API extraction for new packages is temporarily disabled"
-  );
+  if (allowedPackages.has(name)) {
+    const info = await analyzeRegistryPackage({ name, version });
+    await storeRegistryPackageInfo({ name, version, info });
+    return info;
+  }
 
-  // const info = await analyzeRegistryPackage({ name, version });
-
-  // await storeRegistryPackageInfo({ name, version, info });
-
-  // return info;
+  throw new Error("FIXME: API extraction for packages is temporarily disabled");
 };
 
 export default getRegistryPackageInfo;
