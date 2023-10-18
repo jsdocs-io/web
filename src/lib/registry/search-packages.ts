d8cc169da8a77115c62335b6eb4f84e423af9f9b
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-10-19 00:17:36 +0200

    feat: update searchPackages
    Add support for links data and size query parameter.

--- src/lib/registry/search-packages.ts
+++ src/lib/registry/search-packages.ts
@@ -9,7 +9,12 @@ const packageResultSchema = z.object({
 	name: z.string(),
 	version: z.string(),
 	description: z.string().catch(''),
-	keywords: z.array(z.string()).catch([])
+	keywords: z.array(z.string()).catch([]),
+	links: z.object({
+		npm: z.string().optional(),
+		homepage: z.string().optional(),
+		repository: z.string().optional()
+	})
 });
 
 export type PackageResult = z.infer<typeof packageResultSchema>;
@@ -24,12 +29,13 @@ const schema = z.object({
 
 export const searchPackages = async (
 	fetch: typeof window.fetch,
-	query: string
+	query: string,
+	size = 20
 ): Promise<PackageResult[]> => {
 	if (!query) {
 		return [];
 	}
-	const response = await fetch(`${endpoint}?text=${query}`);
+	const response = await fetch(`${endpoint}?text=${query}&size=${size}`);
 	const data = await response.json();
 	return schema
 		.parse(data)
