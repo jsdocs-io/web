ed56a4831300549f1a992fd321c344252722fe67
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-10-19 00:24:37 +0200

    refactor: empty package layout

--- src/routes/package/[...name=name]/+layout.svelte
+++ src/routes/package/[...name=name]/+layout.svelte
@@ -1,12 +1,1 @@
-<script lang="ts">
-	import PackageInfo from '$lib/components/PackageInfo.svelte';
-	import type { LayoutData } from './$types';
-
-	export let data: LayoutData;
-
-	$: ({ packageInfo } = data);
-</script>
-
-<PackageInfo {packageInfo} />
-
 <slot />
