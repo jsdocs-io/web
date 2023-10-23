1d0f6894c826a95e9bdb3182e6f0074bd0ca7e58
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-10-23 16:54:32 +0200

    refactor: remove unused fields from manifest schema

--- src/lib/registry/manifest.ts
+++ src/lib/registry/manifest.ts
@@ -1,23 +1,14 @@
-import { repositorySchema } from '$lib/registry/repository';
 import { z } from 'zod';
 
 export const manifestSchema = z.object({
 	name: z.string(),
 	version: z.string(),
 	description: z.string().optional(),
-	keywords: z.array(z.string()).catch([]),
-	homepage: z.string().optional(),
 	license: z.string().optional(),
-	type: z.string().optional(),
 	types: z.string().optional(),
 	typings: z.string().optional(),
-	sideEffects: z.boolean().optional(),
-	repository: repositorySchema.optional(),
 	dependencies: z.record(z.string()).catch({}),
-	devDependencies: z.record(z.string()).catch({}),
-	peerDependencies: z.record(z.string()).catch({}),
 	dist: z.object({
-		fileCount: z.number().optional(),
 		unpackedSize: z.number().optional()
 	})
 });
