bea895234148e530fedbc9e298c4620eed4ef40e
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-10-23 17:33:23 +0200

    refactor: remove unused Repository type

--- src/lib/registry/repository.ts
+++
@@ -1,9 +0,0 @@
-import { z } from 'zod';
-
-export const repositorySchema = z.object({
-	type: z.string(),
-	url: z.string(),
-	directory: z.string().optional()
-});
-
-export type Repository = z.infer<typeof repositorySchema>;
