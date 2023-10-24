65e42b975a2e63f3ac559071b92cdc53ffec2043
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-10-24 17:59:41 +0200

    refactor: update navbar and footer layout

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -4,12 +4,12 @@
 	import PrivacyLinkButton from '$lib/components/PrivacyLinkButton.svelte';
 </script>
 
-<div class="flex-none">
-	<div class="mx-auto max-w-screen-4xl">
-		<div class="flex items-center justify-start gap-2 overflow-x-auto p-4 sm:justify-end">
-			<AboutLinkButton />
-			<PrivacyLinkButton />
-			<PoweredByVercelLinkButton />
-		</div>
+<div class="flex flex-none justify-center">
+	<div
+		class="flex max-w-screen-4xl grow items-center justify-start gap-2 overflow-x-auto p-4 sm:justify-end"
+	>
+		<AboutLinkButton />
+		<PrivacyLinkButton />
+		<PoweredByVercelLinkButton />
 	</div>
 </div>
--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -6,17 +6,17 @@
 	import ThemeButton from '$lib/components/ThemeButton.svelte';
 </script>
 
-<div class="flex-none">
-	<div class="mx-auto max-w-screen-4xl">
-		<div class="flex items-center justify-between gap-2 overflow-x-auto p-4">
-			<HomepageLinkButton />
+<div class="flex flex-none justify-center">
+	<div
+		class="flex max-w-screen-4xl grow items-center justify-between gap-2 overflow-x-auto p-4"
+	>
+		<HomepageLinkButton />
 
-			<div class="flex items-center gap-2">
-				<FaqLinkButton />
-				<SponsorLinkButton />
-				<GitHubLinkButton />
-				<ThemeButton />
-			</div>
+		<div class="flex items-center gap-2">
+			<FaqLinkButton />
+			<SponsorLinkButton />
+			<GitHubLinkButton />
+			<ThemeButton />
 		</div>
 	</div>
 </div>
