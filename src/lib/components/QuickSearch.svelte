b164a524e806c44d9d4daf2f5e6ce17c470683bb
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-10-29 20:37:23 +0100

    feat: clear quick search query on dialog close

--- src/lib/components/QuickSearch.svelte
+++ src/lib/components/QuickSearch.svelte
@@ -2,12 +2,18 @@
 	import IconClose from '~icons/material-symbols/close';
 	import IconSearch from '~icons/material-symbols/search';
 
+	let query = '';
+
 	const closeQuickSearch = () => {
 		document.querySelector<HTMLDialogElement>('#quick-search')?.close();
 	};
+
+	const clearQuery = () => {
+		query = '';
+	};
 </script>
 
-<dialog id="quick-search" class="modal" open>
+<dialog id="quick-search" class="modal" on:close={clearQuery}>
 	<div class="modal-box p-0">
 		<div class="flex items-center border-b border-base-content/50">
 			<div class="flex w-12 flex-none items-center justify-center">
@@ -20,13 +26,14 @@
 				type="text"
 				placeholder="Quick search..."
 				class="input grow rounded-none border-none px-0 placeholder-base-content focus-within:outline-none focus:outline-none"
+				bind:value={query}
 			/>
 
 			<div class="flex w-12 flex-none items-center justify-center">
 				<button
 					class="btn btn-square btn-ghost btn-sm"
 					on:click={closeQuickSearch}
-					title="Close quick search dialog (Hotkey: Esc)"
+					title="Close quick search (Hotkey: Esc)"
 				>
 					<IconClose class="h-5 w-5" />
 				</button>
