44741d2c18ea08e5a3d47068c5c519270cfe20aa
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2023-10-31 23:39:50 +0100

    fix: re-enable analysis for all packages

--- src/lib/get-registry-package-info.ts
+++ src/lib/get-registry-package-info.ts
@@ -7,37 +7,37 @@ import {
   storeRegistryPackageInfo,
 } from "./registry-package-info-storage";
 
-const allowedPackages = new Set([
-  "@jsdocs-io/extractor",
-  "@types/react",
-  "@unocss/core",
-  "@violentmonkey/shortcut",
-  "axios",
-  "bundle-require",
-  "class-transformer",
-  "class-validator",
-  "depextract",
-  "enttec-open-dmx-usb",
-  "exome",
-  "faastjs",
-  "got",
-  "h3",
-  "hn-ts",
-  "iron-webcrypto",
-  "next",
-  "prettier",
-  "query-registry",
-  "react-router-dom",
-  "short-time-ago",
-  "string-ts",
-  "talkify-tts-api",
-  "tinyargs",
-  "verify-hcaptcha",
-  "vite",
-  "vue",
-  "yup",
-  "zod",
-]);
+// const allowedPackages = new Set([
+//   "@jsdocs-io/extractor",
+//   "@types/react",
+//   "@unocss/core",
+//   "@violentmonkey/shortcut",
+//   "axios",
+//   "bundle-require",
+//   "class-transformer",
+//   "class-validator",
+//   "depextract",
+//   "enttec-open-dmx-usb",
+//   "exome",
+//   "faastjs",
+//   "got",
+//   "h3",
+//   "hn-ts",
+//   "iron-webcrypto",
+//   "next",
+//   "prettier",
+//   "query-registry",
+//   "react-router-dom",
+//   "short-time-ago",
+//   "string-ts",
+//   "talkify-tts-api",
+//   "tinyargs",
+//   "verify-hcaptcha",
+//   "vite",
+//   "vue",
+//   "yup",
+//   "zod",
+// ]);
 
 const getRegistryPackageInfo = async ({
   name,
@@ -51,13 +51,13 @@ const getRegistryPackageInfo = async ({
     return cachedInfo;
   }
 
-  if (allowedPackages.has(name)) {
-    const info = await analyzeRegistryPackage({ name, version });
-    await storeRegistryPackageInfo({ name, version, info });
-    return info;
-  }
+  // if (allowedPackages.has(name)) {
+  const info = await analyzeRegistryPackage({ name, version });
+  await storeRegistryPackageInfo({ name, version, info });
+  return info;
+  // }
 
-  throw new Error("FIXME: API extraction for packages is temporarily disabled");
+  // throw new Error("FIXME: API extraction for packages is temporarily disabled");
 };
 
 export default getRegistryPackageInfo;
