9980bdc0008c5a132b73416f474aba85104deec8
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-02-28 20:13:51 +0100

    build: setup @tailwindcss/typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-import daisyui from "daisyui";
-
 /** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
 		extend: {},
 	},
-	plugins: [daisyui],
+	plugins: [require("@tailwindcss/typography"), require("daisyui")],
 	daisyui: {
 		logs: false,
 	},
