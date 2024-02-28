5784f7099c80a5654d3ed14802a0a6291f14237f
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-02-28 20:54:50 +0100

    build: setup vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
+import process from "node:process";
 
 // https://astro.build/config
 export default defineConfig({
+	output: "hybrid",
+	adapter: vercel({
+		// During the build process on Vercel, a `bun` binary is already available.
+		// Including it in the build output, copies it to serverless functions
+		// making it available at `/var/task/bun1/bun` for Node.js functions.
+		includeFiles: process.env.VERCEL ? ["/bun1/bun"] : [],
+	}),
 	integrations: [tailwind()],
 });
