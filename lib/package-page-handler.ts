4554d8c79a883d775129aac626cd84305f3673c9
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-03-08 20:26:04 +0100

    feat: add initial packagePageHandler

---
+++ lib/package-page-handler.ts
@@ -0,0 +1,20 @@
+import { Effect } from "effect";
+import { packageSlugPath } from "./package-slug-path";
+import { parsePackageSlug } from "./parse-package-slug";
+import { resolvePackage } from "./resolve-package";
+
+export const packagePageHandler = (slug = "") =>
+	Effect.runPromise(Effect.scoped(packagePageHandlerEffect(slug)));
+
+export const packagePageHandlerEffect = (slug = "") =>
+	Effect.gen(function* (_) {
+		const { pkg, pkgName, subpath } = yield* _(parsePackageSlug(slug));
+		const resolvedPkg = yield* _(resolvePackage({ pkg, pkgName }));
+		if (pkg !== resolvedPkg) {
+			return redirect(packageSlugPath({ resolvedPkg, subpath }));
+		}
+		return { pkg, pkgName, subpath };
+	});
+
+const redirect = (path: string) =>
+	new Response(null, { status: 302, headers: { Location: path } });
