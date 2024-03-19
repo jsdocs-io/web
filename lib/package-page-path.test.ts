3c27f5df89a266492b5c7700f75148bd1ad830fd
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-03-19 18:20:13 +0100

    test: fix packagePagePath tests

--- lib/package-page-path.test.ts
+++ lib/package-page-path.test.ts
@@ -2,25 +2,23 @@ import { expect, test } from "vitest";
 import { packagePagePath } from "./package-page-path";
 
 test("bare package, root subpath", () => {
-	expect(packagePagePath({ resolvedPkg: "foo", subpath: "." })).toMatchInlineSnapshot(
-		`"/package/foo"`,
-	);
+	expect(packagePagePath({ pkg: "foo", subpath: "." })).toMatchInlineSnapshot(`"/package/foo"`);
 });
 
 test("scoped package, root subpath", () => {
-	expect(packagePagePath({ resolvedPkg: "@foo/bar", subpath: "." })).toMatchInlineSnapshot(
+	expect(packagePagePath({ pkg: "@foo/bar", subpath: "." })).toMatchInlineSnapshot(
 		`"/package/@foo/bar"`,
 	);
 });
 
 test("bare package, other subpath", () => {
-	expect(packagePagePath({ resolvedPkg: "foo", subpath: "my/subpath" })).toMatchInlineSnapshot(
+	expect(packagePagePath({ pkg: "foo", subpath: "my/subpath" })).toMatchInlineSnapshot(
 		`"/package/foo/my/subpath"`,
 	);
 });
 
 test("scoped package, other subpath", () => {
-	expect(packagePagePath({ resolvedPkg: "@foo/bar", subpath: "my/subpath" })).toMatchInlineSnapshot(
+	expect(packagePagePath({ pkg: "@foo/bar", subpath: "my/subpath" })).toMatchInlineSnapshot(
 		`"/package/@foo/bar/my/subpath"`,
 	);
 });
