89e2ffe24422bd2615f5bb0b248630d3edf6758e
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-03-19 18:27:10 +0100

    fix: return boolean from isValidLicense and add tests

--- lib/is-valid-license.ts
+++ lib/is-valid-license.ts
@@ -1,5 +1,5 @@
-export const isValidLicense = (s = "") => {
+export const isValidLicense = (s = ""): boolean => {
 	// See https://docs.npmjs.com/cli/v10/configuring-npm/package-json#license
 	const license = s.toLowerCase();
-	return license && license !== "unlicensed" && !license.startsWith("see ");
+	return !!license && license !== "unlicensed" && !license.startsWith("see ");
 };
