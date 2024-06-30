dba18dce14702657fc484b8be1a8605bf1325c44
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-06-30 02:00:18 +0200

    feat: add initial quick search dialog

--- src/scripts/alpine.ts
+++ src/scripts/alpine.ts
@@ -1,12 +1,49 @@
 import type { Alpine } from "alpinejs";
 import { formatDistanceStrict } from "date-fns";
 
+type QuickSearchStore = {
+	dialog: HTMLDialogElement | undefined;
+	isOpen: boolean;
+	query: string;
+	init(): void;
+	open(): void;
+	close(): void;
+};
+
 export default (Alpine: Alpine) => {
 	timeAgo(Alpine);
+	quickSearchStore(Alpine);
 };
 
 const timeAgo = (Alpine: Alpine) => {
 	Alpine.data("timeAgo", (timestamp: string) => ({
 		text: formatDistanceStrict(new Date(timestamp), new Date(), { addSuffix: true }),
 	}));
+};
+
+const quickSearchStore = (Alpine: Alpine) => {
+	Alpine.store("quickSearch", {
+		dialog: undefined,
+		isOpen: false,
+		query: "",
+		init() {
+			this.dialog = (document.querySelector("#quick-search") ?? undefined) as any;
+			console.log("init");
+		},
+		open() {
+			if (this.dialog && !this.dialog.open) {
+				this.isOpen = true;
+				this.dialog.showModal();
+				console.log("open");
+			}
+		},
+		close() {
+			if (this.dialog) {
+				this.dialog.close();
+				this.query = "";
+				this.isOpen = false;
+				console.log("close");
+			}
+		},
+	} as QuickSearchStore);
 };
