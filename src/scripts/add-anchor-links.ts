a18cc2c1ec7dada826551a36ac342274316469ef
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-07-04 21:20:57 +0200

    fix: only add anchor links to article headings

--- src/scripts/add-anchor-links.ts
+++ src/scripts/add-anchor-links.ts
@@ -2,5 +2,5 @@ import AnchorJS from "anchor-js";
 
 const anchors = new AnchorJS({ class: "no-underline" });
 document.addEventListener("DOMContentLoaded", () => {
-	anchors.add("h2, h3");
+	anchors.add("article h2, article h3");
 });
