839aaab9406475c4d331c8001e06d58039388bf3
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-07-05 02:07:50 +0200

    feat: update quick search

---
+++ lib/mod.ts
@@ -0,0 +1,1 @@
+export const mod = (a: number, b: number): number => ((a % b) + b) % b;
--- src/scripts/alpine.ts
+++ src/scripts/alpine.ts
@@ -1,18 +1,11 @@
 import type { Alpine } from "alpinejs";
 import { formatDistanceStrict } from "date-fns";
-
-type QuickSearchStore = {
-	dialog: HTMLDialogElement | undefined;
-	isOpen: boolean;
-	query: string;
-	init(): void;
-	open(): void;
-	close(): void;
-};
+import { mod } from "../../lib/mod";
 
 export default (Alpine: Alpine) => {
 	timeAgo(Alpine);
-	quickSearchStore(Alpine);
+	quickSearchOpener(Alpine);
+	quickSearch(Alpine);
 };
 
 const timeAgo = (Alpine: Alpine) => {
@@ -21,29 +14,92 @@ const timeAgo = (Alpine: Alpine) => {
 	}));
 };
 
-const quickSearchStore = (Alpine: Alpine) => {
-	Alpine.store("quickSearch", {
-		dialog: undefined,
-		isOpen: false,
-		query: "",
-		init() {
-			this.dialog = (document.querySelector("#quick-search") ?? undefined) as any;
-			console.log("init");
-		},
-		open() {
-			if (this.dialog && !this.dialog.open) {
-				this.isOpen = true;
-				this.dialog.showModal();
-				console.log("open");
-			}
-		},
-		close() {
-			if (this.dialog) {
-				this.dialog.close();
-				this.query = "";
-				this.isOpen = false;
-				console.log("close");
-			}
-		},
-	} as QuickSearchStore);
+type QuickSearchOpener = {
+	dialog: HTMLDialogElement | undefined;
+	init(): void;
+	open(): void;
+};
+
+const quickSearchOpener = (Alpine: Alpine) => {
+	Alpine.data(
+		"quickSearchOpener",
+		() =>
+			({
+				dialog: undefined,
+				init() {
+					this.dialog = (document.querySelector("#quick-search") ?? undefined) as any;
+				},
+				open() {
+					if (this.dialog && !this.dialog.open) {
+						this.dialog.showModal();
+					}
+				},
+			}) as QuickSearchOpener,
+	);
+};
+
+type QuickSearch = {
+	$refs: {
+		quickSearchResults: HTMLUListElement;
+	};
+	$watch(target: string, callback: () => void): void;
+	dialog: HTMLDialogElement | undefined;
+	query: string;
+	declarations: number[];
+	cursor: number;
+	results: number[];
+	init(): void;
+	close(): void;
+	prevResult(): void;
+	nextResult(): void;
+	useResult(): void;
+	focusResult(): void;
+};
+
+const quickSearch = (Alpine: Alpine) => {
+	Alpine.data(
+		"quickSearch",
+		() =>
+			({
+				dialog: undefined,
+				query: "",
+				declarations: Array.from({ length: 100 }, (_, i) => i + 1),
+				cursor: 0,
+				get results() {
+					if (!this.query) {
+						return this.declarations;
+					}
+					return this.declarations.filter((declaration) => declaration > Number(this.query));
+				},
+				init() {
+					this.dialog = (document.querySelector("#quick-search") ?? undefined) as any;
+					this.$watch("query", () => {
+						this.cursor = 0;
+					});
+					this.$watch("cursor", () => {
+						this.focusResult();
+					});
+				},
+				close() {
+					if (this.dialog) {
+						this.dialog.close();
+						this.query = "";
+					}
+				},
+				prevResult() {
+					this.cursor = mod(this.cursor - 1, this.results.length);
+				},
+				nextResult() {
+					this.cursor = mod(this.cursor + 1, this.results.length);
+				},
+				useResult() {
+					console.log("useResult");
+				},
+				focusResult() {
+					this.$refs.quickSearchResults.children[this.cursor + 1]?.scrollIntoView({
+						block: "nearest",
+					});
+				},
+			}) as QuickSearch,
+	);
 };
