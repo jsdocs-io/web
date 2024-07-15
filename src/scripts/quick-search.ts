d30dcb036c8a629350677cfd3dbea1f129ea5fae
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-07-15 20:12:57 +0200

    refactor: move quick search to own module

---
+++ src/scripts/quick-search.ts
@@ -0,0 +1,124 @@
+import type { AllExtractedDeclarationKind } from "@jsdocs-io/extractor";
+import type { Alpine } from "alpinejs";
+import Fuse from "fuse.js/basic";
+import { mod } from "../../lib/mod";
+import { shortKind } from "../../lib/short-kind";
+
+type QuickSearchOpener = {
+	dialog: HTMLDialogElement | undefined;
+	list: HTMLUListElement | undefined;
+	init(): void;
+	open(): void;
+	cmdSymbol(): string;
+};
+
+export const quickSearchOpener = (Alpine: Alpine) => {
+	Alpine.data(
+		"quickSearchOpener",
+		() =>
+			({
+				dialog: undefined,
+				init() {
+					this.dialog = (document.querySelector("#quick-search") ?? undefined) as any;
+					this.list = (document.querySelector("#quick-search-results") ?? undefined) as any;
+				},
+				open() {
+					if (this.dialog && !this.dialog.open) {
+						this.dialog.showModal();
+						this.list?.children[1]?.scrollIntoView({ block: "nearest" });
+					}
+				},
+				cmdSymbol(): string {
+					return navigator.userAgent.includes("Mac") ? "⌘" : "Ctrl";
+				},
+			}) as QuickSearchOpener,
+	);
+};
+
+type QuickSearch = {
+	$watch(target: string, callback: () => void): void;
+	dialog: HTMLDialogElement | undefined;
+	list: HTMLUListElement | undefined;
+	query: string;
+	cursor: number;
+	fuse: Fuse<QuickSearchDeclaration>;
+	declarations: QuickSearchDeclaration[];
+	results: QuickSearchDeclaration[];
+	init(): void;
+	close(): void;
+	prevResult(): void;
+	nextResult(): void;
+	useResult(): void;
+	focusResult(): void;
+};
+
+type QuickSearchDeclaration = {
+	headingId: string;
+	declarationId: string;
+	kind: string;
+};
+
+export const quickSearch = (Alpine: Alpine) => {
+	Alpine.data(
+		"quickSearch",
+		() =>
+			({
+				dialog: undefined,
+				list: undefined,
+				query: "",
+				cursor: 0,
+				fuse: new Fuse([] as QuickSearchDeclaration[]),
+				declarations: [] as QuickSearchDeclaration[],
+				get results() {
+					if (!this.query) {
+						return this.declarations;
+					}
+					return this.fuse.search(this.query).map(({ item }) => item);
+				},
+				init() {
+					this.dialog = (document.querySelector("#quick-search") ?? undefined) as any;
+					this.list = (document.querySelector("#quick-search-results") ?? undefined) as any;
+					const collator = new Intl.Collator("en");
+					this.declarations = [
+						...(document.querySelectorAll(
+							"h3[data-declaration]",
+						) as NodeListOf<HTMLHeadingElement>),
+					]
+						.map((node) => ({
+							headingId: node.id,
+							declarationId: node.dataset.declaration!,
+							kind: shortKind(node.dataset.kind as AllExtractedDeclarationKind),
+						}))
+						.sort((a, b) => collator.compare(a.headingId, b.headingId));
+					this.fuse = new Fuse(this.declarations, { keys: ["headingId", "kind"] });
+					this.$watch("query", () => {
+						this.cursor = 0;
+					});
+					this.$watch("cursor", () => {
+						this.focusResult();
+					});
+				},
+				close() {
+					this.query = "";
+					this.cursor = 0;
+					this.dialog?.close();
+				},
+				prevResult() {
+					this.cursor = mod(this.cursor - 1, this.results.length);
+				},
+				nextResult() {
+					this.cursor = mod(this.cursor + 1, this.results.length);
+				},
+				useResult() {
+					const headingId = this.results[this.cursor]?.headingId;
+					if (headingId) {
+						window.location.hash = headingId;
+						this.close();
+					}
+				},
+				focusResult() {
+					this.list?.children[this.cursor + 1]?.scrollIntoView({ block: "nearest" });
+				},
+			}) as QuickSearch,
+	);
+};
