54d75fe0196d95c1483bd3558c40285c34be05bd
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-08-22 01:11:44 +0200

    build: add rollup-plugin-visualizer

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import vercel from "@astrojs/vercel/serverless";
 import { defineConfig } from "astro/config";
 import { execSync } from "node:child_process";
 import process from "node:process";
+import { visualizer } from "rollup-plugin-visualizer";
 import Icons from "unplugin-icons/vite";
 
 // https://astro.build/config
@@ -21,7 +22,7 @@ export default defineConfig({
 		:	node({ mode: "standalone" }),
 	integrations: [alpinejs({ entrypoint: "/src/scripts/alpine" }), tailwind()],
 	vite: {
-		plugins: [Icons({ compiler: "astro" })],
+		plugins: [Icons({ compiler: "astro" }), visualizer()],
 		define: {
 			_GIT_COMMIT: JSON.stringify(execSync("git rev-parse HEAD").toString().trim()),
 		},
