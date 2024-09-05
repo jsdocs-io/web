7c078700d2c3194cb15c42075be3de71ea2f9011
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2024-09-05 21:08:54 +0200

    feat: render anchor links on server

--- src/scripts/add-anchor-links.ts
+++
@@ -1,6 +0,0 @@
-import AnchorJS from "anchor-js";
-
-const anchors = new AnchorJS({ class: "no-underline" });
-document.addEventListener("DOMContentLoaded", () => {
-	anchors.add("article h2, article h3");
-});
