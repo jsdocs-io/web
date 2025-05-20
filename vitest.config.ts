b05cc90f715ab876f09fd38ab43f24682f86a43a
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-05-21 00:05:21 +0200

    test: ignore coverage for generated shiki.bundle.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,6 +1,6 @@
 /// <reference types="vitest" />
 import { getViteConfig } from "astro/config";
-import { configDefaults } from "vitest/config";
+import { configDefaults, coverageConfigDefaults } from "vitest/config";
 
 export default getViteConfig({
 	// @ts-ignore
@@ -8,6 +8,7 @@ export default getViteConfig({
 		pool: "threads",
 		coverage: {
 			include: ["lib/**"],
+			exclude: ["lib/shiki.bundle.ts", ...coverageConfigDefaults.exclude],
 		},
 		exclude: [...configDefaults.exclude, "old/**"],
 		testTimeout: 30000,
