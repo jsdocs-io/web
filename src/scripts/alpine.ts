3e20dbd365cf606cf333814929cf0775cb695350
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-05-28 20:34:53 +0200

    chore: remove unused package search alpine component

--- src/scripts/alpine.ts
+++ src/scripts/alpine.ts
@@ -1,12 +1,10 @@
 import type { Alpine } from "alpinejs";
 import { dialogOpener } from "./dialog-opener";
-import { packageSearch } from "./package-search";
 import { quickSearch } from "./quick-search";
 import { timeAgo } from "./time-ago";
 
 export default (Alpine: Alpine) => {
 	Alpine.data("timeAgo", timeAgo);
 	Alpine.data("dialogOpener", dialogOpener);
-	Alpine.data("packageSearch", packageSearch);
 	Alpine.data("quickSearch", quickSearch);
 };
