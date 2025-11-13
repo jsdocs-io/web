acc6c8f888c6dab0ab66172e46349f38c3f02d98
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2025-11-13 16:43:24 +0100

    feat: add ThemeButton

---
+++ src/components/ThemeButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import LucideContrast from "~icons/lucide/contrast";
+
+	function toggleTheme() {
+		if (document.documentElement.classList.contains("dark")) {
+			localStorage.theme = "light";
+			document.documentElement.classList.remove("dark");
+		} else {
+			localStorage.theme = "dark";
+			document.documentElement.classList.add("dark");
+		}
+	}
+</script>
+
+<button
+	type="button"
+	class="btn-sm-icon-outline"
+	title="Change theme"
+	onclick={() => {
+		toggleTheme();
+	}}
+>
+	<LucideContrast class="size-4" />
+	<span class="sr-only">Change theme</span>
+</button>
