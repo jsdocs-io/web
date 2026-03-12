9c8317880dc5b721a76412e13aead005d71b4a90
Edoardo Scibona <12040076+velut@users.noreply.github.com>
2026-03-12 18:25:49 +0100

    chore: ignore no-misleading-character-class lint rule

--- lib/md-to-html.ts
+++ lib/md-to-html.ts
@@ -2,6 +2,7 @@ import { marked } from "marked";
 import { domPurify } from "./dom-purify";
 
 export const mdToHtml = async (md: string): Promise<string> => {
+	/* oxlint-disable no-misleading-character-class */
 	const withoutZeroWidth = md.replace(/^[\u200B\u200C\u200D\u200E\u200F\uFEFF]/, "");
 	const unsafeMd = await marked.parseInline(withoutZeroWidth);
 	return domPurify.sanitize(unsafeMd);
